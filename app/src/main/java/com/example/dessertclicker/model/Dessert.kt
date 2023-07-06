91af16b279ad504dbf1c8a5d8e4c29fec2776235
CheoR <cheo.roman+github@gmail.com>
2023-07-06 14:32:36 -0500

    build: Dessert data model

---
+++ app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.model
+
+/**
+ * [Dessert] data class represents Dessert
+ *  imageId
+ *  price
+ *  startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
